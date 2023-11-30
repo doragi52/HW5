98bf0ccb828620c564aa08e03b77bc6f7f6ecf51
doragi52 <zerg3107@naver.com>
2023-11-30 15:37:06 +0900

    syscall

--- user.h
+++ user.h
@@ -26,6 +26,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int thread_create(void (*start_routine)(void *,void*), void * arg1, void * arg2);
+int thread_join();
 
 // ulib.c
 int stat(const char*, struct stat*);
@@ -40,3 +42,5 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+int thread_create(void (*start_routine)(void *,void*), void * arg1, void * arg2);
+int thread_join();
